f207426b44a6d5ee48752ec44df60ab92efbba0a
acheckler <allisonheckler@gmail.com>
2021-02-09 19:05:56 -0600

    right panel done

--- src/Cart.js
+++ src/Cart.js
@@ -1,24 +1,15 @@
 import React, { Component } from 'react';
 import Total from './Total';
 import Option from './Option';
-
-const USCurrencyFormat = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD'
-  });
   
 
 class Cart extends Component {
     render(){
-        const total = Object.keys(this.props.selected).reduce(
-            (acc, curr) => acc + this.props.selected[curr].cost,
-            0
-          );
         return (
             <section className="main__summary">
             <h2>Your cart</h2>
             <Option selected={this.props.selected} />
-            {/* <Total /> */}
+            <Total selected={this.props.selected}/>
           </section>
         )
 
