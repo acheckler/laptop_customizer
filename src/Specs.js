8583af67ba4830dfdf822f362ed00a0295f47707
acheckler <allisonheckler@gmail.com>
2021-02-09 19:28:15 -0600

    specs component working

--- src/Specs.js
+++ src/Specs.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import slugify from 'slugify';
 
 const USCurrencyFormat = new Intl.NumberFormat('en-US', {
     style: 'currency',
@@ -7,13 +8,38 @@ const USCurrencyFormat = new Intl.NumberFormat('en-US', {
 
 class Specs extends Component {
     render() {
+        const features = Object.keys(this.props.features).map((feature, idx) => {
+            const featureHash = feature + '-' + idx;
+            const options = this.props.features[feature].map(item => {
+              const itemHash = slugify(JSON.stringify(item));
+              return (
+                <div key={itemHash} className="feature__item">
+                  <input
+                    type="radio"
+                    id={itemHash}
+                    className="feature__option"
+                    name={slugify(feature)}
+                    checked={item.name === this.props.selected[feature].name}
+                    onChange={e => this.props.updateFeature(feature, item)}
+                  />
+                  <label htmlFor={itemHash} className="feature__label">
+                    {item.name} ({USCurrencyFormat.format(item.cost)})
+                  </label>
+                </div>
+              );
+            });
+      
             return (
-                <fieldset className="feature" key={featureHash}>
-                  <legend className="feature__name">
-                    <h3>{feature}</h3>
-                  </legend>
-                  {options}
-                </fieldset>
+              <fieldset className="feature" key={featureHash}>
+                <legend className="feature__name">
+                  <h3>{feature}</h3>
+                </legend>
+                {options}
+              </fieldset>
+            );
+          }); 
+            return(
+                features
               );
     }
 }
