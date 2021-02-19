268c2a40c05210945987038331f69f959511c3cd
acheckler <allisonheckler@gmail.com>
2021-02-18 18:46:51 -0600

    fixed parts component

--- src/MainForm.js
+++ src/MainForm.js
@@ -1,11 +1,6 @@
 import React, { Component } from 'react';
-import slugify from 'slugify';
 import Specs from './Specs';
 
-const USCurrencyFormat = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD'
-  });
 
 class MainForm extends Component {
     render() {
--- src/Specs.js
+++ src/Specs.js
@@ -1,47 +1,37 @@
-import React, { Component } from 'react';
-import slugify from 'slugify';
+import React, { Component } from "react";
+import slugify from "slugify";
+import Parts from "./Parts";
 
-const USCurrencyFormat = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD'
-  });
 
 class Specs extends Component {
-    render() {
-        const features = Object.keys(this.props.features).map((feature, idx) => {
-            const featureHash = feature + '-' + idx;
-            const options = this.props.features[feature].map(item => {
-              const itemHash = slugify(JSON.stringify(item));
-              return (
-                <div key={itemHash} className="feature__item">
-                  <input
-                    type="radio"
-                    id={itemHash}
-                    className="feature__option"
-                    name={slugify(feature)}
-                    checked={item.name === this.props.selected[feature].name}
-                    onChange={e => this.props.updateFeature(feature, item)}
-                  />
-                  <label htmlFor={itemHash} className="feature__label">
-                    {item.name} ({USCurrencyFormat.format(item.cost)})
-                  </label>
-                </div>
-              );
-            });
-      
-            return (
-              <fieldset className="feature" key={featureHash}>
-                <legend className="feature__name">
-                  <h3>{feature}</h3>
-                </legend>
-                {options}
-              </fieldset>
-            );
-          }); 
-            return(
-                features
-              );
-    }
+  render() {
+    const features = Object.keys(this.props.features).map((feature, idx) => {
+      const featureHash = feature + "-" + idx;
+      const options = this.props.features[feature].map((item) => {
+        const itemHash = slugify(JSON.stringify(item));
+        return (
+          <Parts
+            features={this.props.features}
+            selected={this.props.selected}
+            updateFeature={this.props.updateFeature}
+            itemHash={itemHash}
+            feature={feature}
+            item={item}
+          />
+        );
+      });
+
+      return (
+        <fieldset className="feature" key={featureHash}>
+          <legend className="feature__name">
+            <h3>{feature}</h3>
+          </legend>
+          {options}
+        </fieldset>
+      );
+    });
+    return features;
+  }
 }
 
-export default Specs
+export default Specs;
